62f74186babfacbcf4eb2152deead96f8fe63be2
Dominik <bumboxica@gmail.com>
2023-03-13 01:35:19 +0100

    Implementation of not working secuity (elements with task backend not showing)

--- backend/src/main/java/pl/management/app/management_app/api/resource/TaskController.java
+++ backend/src/main/java/pl/management/app/management_app/api/resource/TaskController.java
@@ -9,7 +9,7 @@ import pl.management.app.management_app.api.repository.TaskRepository;
 import java.util.List;
 import java.util.Optional;
 
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("api")
 public class TaskController {
