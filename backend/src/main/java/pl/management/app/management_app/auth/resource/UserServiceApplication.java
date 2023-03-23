d7a4e38d75ab6db2a9c4d553c88c2490175cbcae
Dominik <bumboxica@gmail.com>
2023-03-23 22:35:12 +0100

    User api in one file with security

--- backend/src/main/java/pl/management/app/management_app/auth/resource/UserServiceApplication.java
+++ backend/src/main/java/pl/management/app/management_app/auth/resource/UserServiceApplication.java
@@ -1,22 +1,20 @@
 package pl.management.app.management_app.auth.resource;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 import pl.management.app.management_app.auth.model.User;
+import pl.management.app.management_app.auth.repository.UserRepository;
 
 
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-@SpringBootApplication
 @RestController
 @CrossOrigin(origins = "*")
 public class UserServiceApplication {
-
+    @Autowired
+    private UserRepository repository;
 
     @GetMapping("/")
     public String login(){
@@ -26,6 +24,16 @@ public class UserServiceApplication {
     public List<User> getUsers(){
         return Stream.of(new User(1,"dominik","123","ADMIN","Dominik", "Lysak","dominik@gmail.com"))
                      /*   new User(101,"Piotr","Lysak","piotr@gmail.com"))*/.collect(Collectors.toList());
+    }
+
+    @PostMapping("/addUser")
+    public String addUser(@RequestBody User user) {
+        repository.save(user);
+        return "Added user with id: " + user.getId();
+    }
+    @GetMapping("/showAllUsers")
+    public List<User> getAllUsers(){
+        return repository.findAll();
     }
 
 
