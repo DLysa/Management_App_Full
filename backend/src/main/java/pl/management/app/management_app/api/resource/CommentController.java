c081adad93196de4f0742175c7f4eaf866829e5a
DLysa <dominik.lysakk@gmail.com>
2024-01-22 03:37:16 +0100

    Comment refresh

--- backend/src/main/java/pl/management/app/management_app/api/resource/CommentController.java
+++ backend/src/main/java/pl/management/app/management_app/api/resource/CommentController.java
@@ -1,6 +1,8 @@
 package pl.management.app.management_app.api.resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pl.management.app.management_app.api.model.Comment;
 import pl.management.app.management_app.api.repository.CommentRepository;
@@ -20,11 +22,15 @@ public class CommentController {
     private SequenceContoller sequenceContoller;
 
     @PostMapping("/addComment")
-    public String addComment(@RequestBody Comment comment) {
-
-        comment.setId(sequenceContoller.getSequenceNumber(SEQUENCE_NAME));
-        repository.save(comment);
-        return "Added comment with id: " + comment.getId();
+    public ResponseEntity<Comment> addComment(@RequestBody Comment comment) {
+        try {
+            comment.setId(sequenceContoller.getSequenceNumber(SEQUENCE_NAME));
+            Comment savedComment = repository.save(comment);
+            return ResponseEntity.status(HttpStatus.CREATED).body(savedComment);
+        } catch (Exception e) {
+            // Log the exception and return an appropriate error response
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 /*
     @GetMapping("/showComment/{taskId}")
